7ad9615e5b991f9faaa57ecb68a1108b6002c535
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-10-31 15:29:29 +0700

    program

--- Server_Rama Soni Ravictor_20180140103/Program.cs
+++ Server_Rama Soni Ravictor_20180140103/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Server_Rama_Soni_Ravictor_20180140103.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -19,4 +21,45 @@ namespace Server_Rama_Soni_Ravictor_20180140103
             Application.Run(new Form1());
         }
     }
+    public class Server
+    {
+        public void OnServer()
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+        public void OffServer()
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(IMatematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Off!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+    }
 }
+
