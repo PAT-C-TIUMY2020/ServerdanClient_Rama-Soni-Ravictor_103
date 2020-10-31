3f7765e6d488ea469e57fe107a6a81d72a693490
Rama Soni Ravictor <55401842+ravictor21@users.noreply.github.com>
2020-10-31 15:21:57 +0700

    Add project files.

---
+++ Server_Rama Soni Ravictor_20180140103/Form1.cs
@@ -0,0 +1,50 @@
+﻿using Server_Rama_Soni_Ravictor_20180140103.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_Rama_Soni_Ravictor_20180140103
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label3.Text = "Klik OFF Untuk Mematikan Server";
+            label2.Text = "Server ON";
+
+            Server server = new Server();
+            server.OnServer();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            label3.Text = "Klik ON Untuk Menjalankan Server";
+            label2.Text = "Server OFF";
+
+            Server server = new Server();
+            server.OffServer();
+        }
+    }
+}
---
+++ ServerdanClient_Rama Soni Ravictor_20180140103/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerdanClient_Rama_Soni_Ravictor_20180140103
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            ServiceReference1.MatematikaClient obj = new ServiceReference1.MatematikaClient();
+            double hasilTambah = obj.Tambah(a, b);
+            label7.Text = hasilTambah.ToString();
+            double hasilKurang = obj.Kurang(a, b);
+            label8.Text = hasilKurang.ToString();
+            double hasilKali = obj.Kali(a, b);
+            label9.Text = hasilKali.ToString();
+            double hasilBagi = obj.Bagi(a, b);
+            label10.Text = hasilBagi.ToString();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
